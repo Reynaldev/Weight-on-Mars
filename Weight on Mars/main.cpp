276605c21c77d4ad98ebb6f2d552737d6d06e329
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2022-08-28 16:55:33 +0700

    Yes

---
+++ Weight on Mars/main.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main()
+{
+    double weight;
+    double totalWeight;
+
+    std::cout << "Input item weight: ";
+    std::cin >> weight;
+
+    totalWeight = weight * 3.711;
+
+    std::cout << "This item weighted: " << totalWeight << "Kg on Mars!";
+
+    return 0;
+}
